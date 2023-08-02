a7ba41adb33863f5dbcf36e6ed0514a22da59a8b
jeyfred <jeyfredc@gmail.com>
2023-08-02 07:23:08 -0500

    Finalizacion mapsApp

--- src/app/maps/pages/markers-page/markers-page.component.ts
+++ src/app/maps/pages/markers-page/markers-page.component.ts
@@ -1,9 +1,115 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { LngLat, Map, Marker } from 'mapbox-gl';
+
+interface MarkerAndColor {
+  color: string;
+  marker: Marker;
+}
+
+interface PlainMarker {
+  color: string;
+  lngLat: number[];
+}
 
 @Component({
   templateUrl: './markers-page.component.html',
-  styleUrls: ['./markers-page.component.css']
+  styleUrls: ['./markers-page.component.css'],
 })
 export class MarkersPageComponent {
+  @ViewChild('map') divMap?: ElementRef;
+
+  markers: MarkerAndColor[] = [];
+
+  zoom: number = 10;
+
+  map?: Map;
+  currentLngLat: LngLat = new LngLat(-74.19337684757431, 4.589712718235575);
+
+  ngAfterViewInit(): void {
+    if (!this.divMap) throw 'El elemento html no fue encontrado';
+    this.map = new Map({
+      /*  En este caso cambiamos el elemento map por la referencia creada arriba como divMap*/
+      container: this.divMap.nativeElement,
+      // container: 'map', // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: this.currentLngLat, // starting position [lng, lat]
+      zoom: 13, // starting zoom
+    });
+
+    this.readFromLocalStorage();
+
+    // const marker = new Marker({
+    //   color:'red'
+    // })
+    // .setLngLat(this.currentLngLat)
+    // .addTo(this.map)
+  }
+
+  createMarker() {
+    if (!this.map) return;
+    const color = '#xxxxxx'.replace(/x/g, (y) =>
+      ((Math.random() * 16) | 0).toString(16)
+    );
+    const lngLat = this.map.getCenter();
+    this.addMarker(lngLat, color);
+  }
+
+  addMarker(lngLat: LngLat, color: string) {
+    if (!this.map) return;
+
+    const marker = new Marker({
+      color: color,
+      draggable: true,
+    })
+      .setLngLat(lngLat)
+      .addTo(this.map);
+
+    this.markers.push({
+      color,
+      marker,
+    });
+    this.saveToLocalStorage();
+
+    marker.on('dragend', () => {
+      this.saveToLocalStorage();
+    });
+  }
+
+  deleteMarker(index: number) {
+    this.markers[index].marker.remove();
+    this.markers.splice(index, 1);
+  }
+
+  flyTo(marker: Marker) {
+    this.map?.flyTo({
+      zoom: 14,
+      center: marker.getLngLat(),
+    });
+  }
+
+  saveToLocalStorage() {
+    const plainMarkers: PlainMarker[] = this.markers.map(
+      ({ color, marker }) => {
+        return {
+          color,
+          lngLat: marker.getLngLat().toArray(),
+        };
+      }
+    );
+    localStorage.setItem('plainMarkers', JSON.stringify(plainMarkers));
+  }
+
+  readFromLocalStorage() {
+    const plainMarkersString = localStorage.getItem('plainMarkers') ?? '[]';
+    const plainMarkers: PlainMarker[] = JSON.parse( plainMarkersString ); //! OJO!
+
+    plainMarkers.forEach( ({ color, lngLat }) => {
+      const [ lng, lat ] = lngLat;
+      const coords = new LngLat( lng, lat );
+
+      this.addMarker( coords, color );
+    })
+
+  }
 
 }
--- src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
+++ src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
@@ -1,28 +1,68 @@
-import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
-import { Map } from 'mapbox-gl';
+import { Component, ElementRef, ViewChild, AfterViewInit, OnDestroy } from '@angular/core';
+import { LngLat, Map } from 'mapbox-gl';
 
 @Component({
   templateUrl: './zoom-range-page.component.html',
-  styleUrls: ['./zoom-range-page.component.css']
+  styleUrls: ['./zoom-range-page.component.css'],
 })
-export class ZoomRangePageComponent implements AfterViewInit{
+export class ZoomRangePageComponent implements AfterViewInit, OnDestroy {
 
-    /* ViewChild es la forma par hacer referencia a un elemento, en este caso es un div que esta 
+  /* ViewChild es la forma par hacer referencia a un elemento, en este caso es un div que esta 
   con referencia #map */
   @ViewChild('map') divMap?: ElementRef;
 
+  zoom: number = 10;
+
+  map?: Map;
+  currentLngLat: LngLat = new LngLat(-74.19337684757431,4.589712718235575)
 
   ngAfterViewInit(): void {
-    if ( !this.divMap) throw 'El elemento html no fue encontrado'  
-    const map = new Map({
+    if (!this.divMap) throw 'El elemento html no fue encontrado';
+    this.map = new Map({
       /*  En este caso cambiamos el elemento map por la referencia creada arriba como divMap*/
       container: this.divMap.nativeElement,
       // container: 'map', // container ID
       style: 'mapbox://styles/mapbox/streets-v12', // style URL
-      center: [-74.5, 40], // starting position [lng, lat]
-      zoom: 9, // starting zoom
-      });
-    
+      center: this.currentLngLat, // starting position [lng, lat]
+      zoom: this.zoom, // starting zoom
+    });
+    this.mapListeners();
   }
 
+  ngOnDestroy(): void {
+    this.map?.remove()
+  }
+
+  mapListeners() {
+    if (!this.map) throw 'Mapa no inicializado';
+
+    this.map.on('zoom', (ev) => {
+      this.zoom = this.map!.getZoom();
+    });
+
+    this.map.on('zoomend', (ev) => {
+      if (this.map!.getZoom() < 18) return;
+      this.map!.zoomTo(18);
+    });
+
+    this.map.on('move', () => {
+      this.currentLngLat = this.map!.getCenter()
+      console.log(this.currentLngLat);
+      
+    });
+
+  }
+
+  zoomIn() {
+    this.map?.zoomIn();
+  }
+
+  zoomOut() {
+    this.map?.zoomOut();
+  }
+
+  zoomChanged( value:string ) {
+      this.zoom = Number(value)
+      this.map?.zoomTo(this.zoom)
+  }
 }
